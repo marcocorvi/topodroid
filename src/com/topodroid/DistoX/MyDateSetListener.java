d35ba735ab8210a0bfb7ecc89fbdc8abb16bf727
marcocorvi <marco.corvi@gmail.com>
2015-10-20 10:57:42 +0200

    strings files cleanup

---
+++ src/com/topodroid/DistoX/MyDateSetListener.java
@@ -0,0 +1,31 @@
+/* @file MyDateSetListener.java
+ *
+ * @author marco corvi
+ * @date may 2012
+ *
+ * @brief TopoDroid date setter
+ * --------------------------------------------------------
+ *  Copyright This sowftare is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ * --------------------------------------------------------
+ */
+package com.topodroid.DistoX;
+
+import android.widget.TextView;
+import android.widget.DatePicker;
+import android.app.DatePickerDialog.OnDateSetListener;
+
+class MyDateSetListener implements OnDateSetListener 
+{
+  TextView mView;
+
+  MyDateSetListener( TextView v )
+  {
+    mView = v;
+  }
+
+  @Override
+  public void onDateSet( DatePicker view, int y, int m, int d ) {
+    mView.setText( TopoDroidUtil.composeDate( y, m, d ) );
+  }
+}
