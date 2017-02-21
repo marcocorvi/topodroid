9925129f342948dc61fbd5017048149b5a98112d
marcocorvi <marco.corvi@gmail.com>
2017-02-21 11:36:33 +0100

    v. 3.3.1a

--- src/com/topodroid/DistoX/TopoDroidApp.java
+++ src/com/topodroid/DistoX/TopoDroidApp.java
@@ -92,7 +92,7 @@ public class TopoDroidApp extends Application
 {
   String mCWD;  // current work directory
 
-  static String SYMBOL_VERSION = "34";
+  static String SYMBOL_VERSION = "35";
   static String VERSION = "0.0.0"; 
   static int VERSION_CODE = 0;
   static int MAJOR = 0;
