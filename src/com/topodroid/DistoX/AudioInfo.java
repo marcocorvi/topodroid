2cbf56d6f12837ea0a39373922f10028ec406062
marcocorvi <marco.corvi@gmail.com>
2017-03-09 07:36:46 +0100

    audio info

---
+++ src/com/topodroid/DistoX/AudioInfo.java
@@ -0,0 +1,39 @@
+/** @file AudioInfo.java
+ *
+ * @author marco corvi
+ * @date may 2012
+ *
+ * @brief TopoDroid Audio info (id, sid, shot_id, date )
+ * --------------------------------------------------------
+ *  Copyright This sowftare is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ * --------------------------------------------------------
+ */
+package com.topodroid.DistoX;
+
+class AudioInfo
+{
+  public long sid;       // survey id
+  public long id;        // photo id
+  public long shotid;    // shot id
+  public String mDate;
+
+  public AudioInfo( long _sid, long _id, long _shotid, String dt )
+  {
+    sid    = _sid;
+    id     = _id;
+    shotid = _shotid;
+    mDate = dt;
+  }
+
+  // String getPhotoName() 
+  // {
+  //   return String.format( "%d-%03d", sid, id );
+  // }
+
+  public String toString()
+  {
+    return id + " <" + mDate  + "> ";
+  }
+
+}
