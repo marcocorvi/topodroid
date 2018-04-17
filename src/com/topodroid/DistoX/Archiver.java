a4de0a0401e2e5b7bd41f5f8766d8f4c124d49b4
marcocorvi <marco.corvi@gmail.com>
2018-04-17 16:26:57 +0200

    v. 4.0.0f

--- src/com/topodroid/DistoX/Archiver.java
+++ src/com/topodroid/DistoX/Archiver.java
@@ -204,122 +204,123 @@ class Archiver
       }
       zip.close();
       TDLog.Log( TDLog.LOG_ZIP, "un-archive manifest " + ok_manifest );
-      if ( ok_manifest == 0 ) {
-        FileInputStream fis = new FileInputStream( filename );
-        ZipInputStream zin = new ZipInputStream( fis );
-        while ( ( ze = zin.getNextEntry() ) != null ) {
-          if ( ze.isDirectory() ) {
-            File dir = new File( TDPath.getDirFile( ze.getName() ) );
-            if ( ! dir.isDirectory() ) {
-              dir.mkdirs();
-            }
-          } else {
-            TDLog.Log( TDLog.LOG_ZIP, "Zip entry \"" + ze.getName() + "\"" );
-            boolean sql = false;
-            pathname = null;
-            if ( ze.getName().equals( "manifest" ) ) {
-              // skip
-            } else if ( ze.getName().equals( "survey.sql" ) ) {
-              pathname = TDPath.getSqlFile();
-              sql = true;
-            } else if ( ze.getName().endsWith( TDPath.CSV ) ) {
-              pathname = TDPath.getCsvFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.CSX ) ) {
-              pathname = TDPath.getCsxFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.CAVE ) ) {
-              pathname = TDPath.getCaveFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.CAV ) ) {
-              pathname = TDPath.getCavFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.DAT ) ) {
-              pathname = TDPath.getDatFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.GRT ) ) {
-              pathname = TDPath.getGrtFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.GTX ) ) {
-              pathname = TDPath.getGtxFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.DXF ) ) {
-              pathname = TDPath.getDxfFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.KML ) ) {
-              pathname = TDPath.getKmlFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.PLT ) ) {
-              pathname = TDPath.getPltFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.PNG ) ) {
-              pathname = TDPath.getPngFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.SRV ) ) {
-              pathname = TDPath.getSrvFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.SVG ) ) {
-              pathname = TDPath.getSvgFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.SVX ) ) {
-              pathname = TDPath.getSvxFile( ze.getName() );
+      if ( ok_manifest < 0 ) return ok_manifest;
 
-            } else if ( ze.getName().endsWith( TDPath.TH ) ) {
-              pathname = TDPath.getThFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.TH2 ) ) {
-              pathname = TDPath.getTh2File( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.TH3 ) ) {
-              pathname = TDPath.getTh3File( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.TDR ) ) {
-              pathname = TDPath.getTdrFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.TDR3 ) ) {
-              pathname = TDPath.getTdr3File( ze.getName() );
-
-            } else if ( ze.getName().endsWith( TDPath.TRB ) ) {
-              pathname = TDPath.getTrbFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.TRO ) ) {
-              pathname = TDPath.getTroFile( ze.getName() );
-            } else if ( ze.getName().endsWith( TDPath.TOP ) ) {
-              pathname = TDPath.getTopFile( ze.getName() );
-
-            } else if ( ze.getName().endsWith( TDPath.TXT ) ) {
-              pathname = TDPath.getNoteFile( ze.getName() );
-
-            } else if ( ze.getName().endsWith( ".wav" ) ) { // AUDIOS
-              pathname = TDPath.getAudioDir( surveyname );
-              File file = new File( pathname );
-              file.mkdirs();
-              pathname = TDPath.getAudioFile( surveyname, ze.getName() );
-            } else if ( ze.getName().endsWith( ".jpg" ) ) { // PHOTOS
-              // FIXME need survey dir
-              pathname = TDPath.getJpgDir( surveyname );
-              File file = new File( pathname );
-              file.mkdirs();
-              pathname = TDPath.getJpgFile( surveyname, ze.getName() );
-            } else {
-              // unexpected file type
-              pathname = null; //
-            }
-            TDLog.Log( TDLog.LOG_ZIP, "Zip filename \"" + pathname + "\"" );
-            if ( pathname != null ) {
-              TDPath.checkPath( pathname );
-              FileOutputStream fout = new FileOutputStream( pathname );
-              int c;
-              while ( ( c = zin.read( buffer ) ) != -1 ) {
-                fout.write(buffer, 0, c);
-              }
-              fout.close();
-              if ( sql ) {
-                TDLog.Log( TDLog.LOG_ZIP, "Zip sqlfile \"" + pathname + "\"" );
-                sql_success = ( app_data.loadFromFile( pathname, app.mManifestDbVersion ) >= 0 );
-                File f = new File( pathname );
-                f.delete();
-              }
-            }
-            zin.closeEntry();
+      FileInputStream fis = new FileInputStream( filename );
+      ZipInputStream zin = new ZipInputStream( fis );
+      while ( ( ze = zin.getNextEntry() ) != null ) {
+        if ( ze.isDirectory() ) {
+          File dir = new File( TDPath.getDirFile( ze.getName() ) );
+          if ( ! dir.isDirectory() ) {
+            dir.mkdirs();
           }
+        } else {
+          TDLog.Log( TDLog.LOG_ZIP, "Zip entry \"" + ze.getName() + "\"" );
+          boolean sql = false;
+          pathname = null;
+          if ( ze.getName().equals( "manifest" ) ) {
+            // skip
+          } else if ( ze.getName().equals( "survey.sql" ) ) {
+            pathname = TDPath.getSqlFile();
+            sql = true;
+          } else if ( ze.getName().endsWith( TDPath.CSV ) ) {
+            pathname = TDPath.getCsvFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.CSX ) ) {
+            pathname = TDPath.getCsxFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.CAVE ) ) {
+            pathname = TDPath.getCaveFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.CAV ) ) {
+            pathname = TDPath.getCavFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.DAT ) ) {
+            pathname = TDPath.getDatFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.GRT ) ) {
+            pathname = TDPath.getGrtFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.GTX ) ) {
+            pathname = TDPath.getGtxFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.DXF ) ) {
+            pathname = TDPath.getDxfFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.KML ) ) {
+            pathname = TDPath.getKmlFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.PLT ) ) {
+            pathname = TDPath.getPltFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.PNG ) ) {
+            pathname = TDPath.getPngFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.SRV ) ) {
+            pathname = TDPath.getSrvFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.SVG ) ) {
+            pathname = TDPath.getSvgFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.SVX ) ) {
+            pathname = TDPath.getSvxFile( ze.getName() );
+
+          } else if ( ze.getName().endsWith( TDPath.TH ) ) {
+            pathname = TDPath.getThFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.TH2 ) ) {
+            pathname = TDPath.getTh2File( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.TH3 ) ) {
+            pathname = TDPath.getTh3File( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.TDR ) ) {
+            pathname = TDPath.getTdrFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.TDR3 ) ) {
+            pathname = TDPath.getTdr3File( ze.getName() );
+
+          } else if ( ze.getName().endsWith( TDPath.TRB ) ) {
+            pathname = TDPath.getTrbFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.TRO ) ) {
+            pathname = TDPath.getTroFile( ze.getName() );
+          } else if ( ze.getName().endsWith( TDPath.TOP ) ) {
+            pathname = TDPath.getTopFile( ze.getName() );
+
+          } else if ( ze.getName().endsWith( TDPath.TXT ) ) {
+            pathname = TDPath.getNoteFile( ze.getName() );
+
+          } else if ( ze.getName().endsWith( ".wav" ) ) { // AUDIOS
+            pathname = TDPath.getAudioDir( surveyname );
+            File file = new File( pathname );
+            file.mkdirs();
+            pathname = TDPath.getAudioFile( surveyname, ze.getName() );
+          } else if ( ze.getName().endsWith( ".jpg" ) ) { // PHOTOS
+            // FIXME need survey dir
+            pathname = TDPath.getJpgDir( surveyname );
+            File file = new File( pathname );
+            file.mkdirs();
+            pathname = TDPath.getJpgFile( surveyname, ze.getName() );
+          } else {
+            // unexpected file type
+            pathname = null; //
+          }
+          TDLog.Log( TDLog.LOG_ZIP, "Zip filename \"" + pathname + "\"" );
+          if ( pathname != null ) {
+            TDPath.checkPath( pathname );
+            FileOutputStream fout = new FileOutputStream( pathname );
+            int c;
+            while ( ( c = zin.read( buffer ) ) != -1 ) {
+              fout.write(buffer, 0, c);
+            }
+            fout.close();
+            if ( sql ) {
+              TDLog.Log( TDLog.LOG_ZIP, "Zip sqlfile \"" + pathname + "\"" );
+              sql_success = ( app_data.loadFromFile( pathname, app.mManifestDbVersion ) >= 0 );
+              File f = new File( pathname );
+              f.delete();
+            }
+          }
+          zin.closeEntry();
         }
-        zin.close();
       }
+      zin.close();
+      
     } catch ( FileNotFoundException e ) {
       TDLog.Error( "ERROR File: " + e.toString() );
     } catch ( IOException e ) {
       TDLog.Error( "ERROR IO: " + e.toString() );
     }
-    if ( ! sql_success ) {
+    if ( ok_manifest == 0 && ! sql_success ) {
       TDLog.Error( "ERROR SQL" );
       // TODO tell user that there was a problem
-      ok_manifest = -5;
+      return -5;
     }
 
-    return ok_manifest;
+    return ok_manifest; // return 0
   }
 }
 
