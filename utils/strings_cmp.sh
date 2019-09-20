3899fc456d00f77a21640f8c42fc42704badf979
marcocorvi <marco.corvi@gmail.com>
2019-09-20 15:26:22 +0200

    fixed DXF references

--- utils/strings_cmp.sh
+++ utils/strings_cmp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 for i in bg cn de fr es hu it pl pt ro ru sk; do
-   echo $i
+   echo -n "$i: "
    ./utils/strings_cmp.pl res/values/strings.xml res/values-$i/strings.xml  | grep Total
 done
 
