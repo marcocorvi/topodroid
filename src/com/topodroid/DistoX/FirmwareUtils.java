2dd3b941c977d22f29865f4480a69d46afac071c
marcocorvi <marco.corvi@gmail.com>
2021-03-01 21:21:17 +0100

    Firmware utils - preliminary 2.6.3 support

--- src/com/topodroid/DistoX/FirmwareUtils.java
+++ src/com/topodroid/DistoX/FirmwareUtils.java
@@ -23,10 +23,10 @@ import java.io.DataInputStream;
 import java.io.IOException;
 // import java.io.FileNotFoundException;
 
-class FirmwareUtils 
+class FirmwareUtils
 {
   // sigmature is 64 bytes after the first 2048
-  static final private byte[] signature = {
+  static final private byte[] signature2 = {
     (byte)0x03, (byte)0x48, (byte)0x85, (byte)0x46, (byte)0x03, (byte)0xf0, (byte)0x34, (byte)0xf8,
     (byte)0x00, (byte)0x48, (byte)0x00, (byte)0x47, (byte)0xf5, (byte)0x08, (byte)0x00, (byte)0x08,
     (byte)0x40, (byte)0x0c, (byte)0x00, (byte)0x20, (byte)0x00, (byte)0x23, (byte)0x02, (byte)0xe0,
@@ -36,6 +36,17 @@ class FirmwareUtils
     (byte)0x35, (byte)0xf8, (byte)0x00, (byte)0xf0, (byte)0x34, (byte)0xf8, (byte)0x24, (byte)0x4e,
     (byte)0x00, (byte)0xf0, (byte)0x30, (byte)0xf8, (byte)0x00, (byte)0x1b, (byte)0x49, (byte)0x1b
   };
+  static final private byte[] signature6 = {
+    (byte)0x09, (byte)0xd1, (byte)0x20, (byte)0x78, (byte)0x09, (byte)0x49, (byte)0x08, (byte)0x70,
+    (byte)0x01, (byte)0x20, (byte)0x07, (byte)0x49, (byte)0x08, (byte)0x60, (byte)0xff, (byte)0x20,
+    (byte)0x03, (byte)0x30, (byte)0x04, (byte)0x49, (byte)0x08, (byte)0x60, (byte)0x64, (byte)0x1c,
+    (byte)0x00, (byte)0xbf, (byte)0x28, (byte)0x46, (byte)0x6d, (byte)0x1e, (byte)0x00, (byte)0x28,
+    (byte)0xac, (byte)0xdc, (byte)0x30, (byte)0xbd, (byte)0x54, (byte)0x01, (byte)0x00, (byte)0x20,
+    (byte)0x50, (byte)0x01, (byte)0x00, (byte)0x20, (byte)0xb0, (byte)0x0a, (byte)0x00, (byte)0x20,
+    (byte)0xf8, (byte)0xb5, (byte)0x07, (byte)0x46, (byte)0x0d, (byte)0x46, (byte)0xee, (byte)0x1c,
+    (byte)0x00, (byte)0x24, (byte)0x03, (byte)0xe0, (byte)0x69, (byte)0x46, (byte)0x0e, (byte)0x55
+  };
+
 
   //                                   2.1   2.2   2.3   2.4  2.4c   2.5  2.5c  2.51
   // signatures differ in bytes 7- 6  f834  f83a  f990  fa0a  fe94  fb7e  fc10  f894
@@ -52,8 +63,26 @@ class FirmwareUtils
   //   return false;
   // }
 
+  private static int computeSignature2( byte[] buf )
+  {
+    for ( int k=0; k<64; ++k ) {
+      if ( k==6 || k==7 || k==12 || k==16 || k==17 ) continue;
+      if ( buf[k] != signature2[k] ) return -k;
+    }
+    return 64; // success
+  }
+
+  private static int computeSignature6( byte[] buf )
+  {
+    for ( int k=0; k<64; ++k ) {
+      if ( buf[k] != signature6[k] ) return -k;
+    }
+    return 64; // success
+  }
+
   // try to guess firmware version reading bytes from the file
-  // return <= 0 (failure) or one of 21 22 23 24 25 250
+  // return <= 0 (failure) or one of
+  //    2100 2200 2300 2400 2412 2500 2501 2512 2630
   //
   static int readFirmwareFirmware( File fp )
   {
@@ -71,52 +100,11 @@ class FirmwareUtils
         // Log.v("DistoX", "failed read");
         return 0;
       }
-      for ( int k=0; k<64; ++k ) {
-        // Log.v("DistoX", "byte " + k + " " + buf[k] + " sign " + signature[k] );
-        if ( k==6 || k==7 || k==12 || k==16 || k==17 ) continue;
-        if ( buf[k] != signature[k] ) return -k;
+      if ( computeSignature2( buf ) == 64 ) {
+        return readFirmware2( buf );
       }
-      //                                   2.1   2.2   2.3   2.4  2.4c   2.5  2.5c  2.51
-      // signatures differ in bytes 7- 6  f834  f83a  f990  fa0a  fe94  fb7e  fc10  fb94
-      //                             -12  08d5  08d5  08d5  08f5  08f5  08d5  08d5  08d5
-      //                           17-16  0c40  0c40  0c50  0c30  0c38  0c40  0c48  0c40
-
-      if ( buf[7] == (byte)0xf8 ) {       // 2.1 2.2
-        if ( buf[6] == (byte)0x34 ) {     // 2.1
-          return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xf5 )? 2100 : -2100;
-        } else if ( buf[6] == (byte)0x3a ) { // 2.2
-          return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xf5 )? 2200 : -2200;
-        }
-        return -200;
-      } else if ( buf[7] == (byte)0xf9 ) { // 2.3
-        if ( buf[6] == (byte)0x90 ) {
-          return (buf[16] == (byte)0x50 && buf[12] == (byte)0xf5 )? 2300 : -2300;
-        }
-        return -230;
-      } else if ( buf[7] == (byte)0xfa ) { // 2.4
-        if ( buf[6] == (byte)0x0a ) {
-          return ( buf[16] == (byte)0x30 && buf[12] == (byte)0xf5 )? 2400 : -2400;
-        }
-        return -240;
-      } else if ( buf[7] == (byte)0xfb ) { // 2.5  2.51
-        if ( buf[6] == (byte)0x7e ) {      // 2.5
-          return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xd5 )? 2500 : -2500;
-        } else if ( buf[6] == (byte)0x94 ) { // 2.51
-          return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xd5 )? 2501 : -2501;
-        }
-        return -250;
-      } else if ( buf[7] == (byte)0xfc ) { // 2.5c
-        if ( buf[6] == (byte)0x10 ) {
-          return ( buf[16] == (byte)0x48 && buf[12] == (byte)0xd5 )? 2512 : -2512;
-        }
-        return -256;
-      } else if ( buf[7] == (byte)0xfe ) { // 2.4c
-        if ( buf[6] == (byte)0x94 ) {
-          return ( buf[16] == (byte)0x38 && buf[12] == (byte)0xf5 )? 2412 : -2412;
-        }
-        return -246;
-      } else {
-        return -27; // failed on byte[7]
+      if ( computeSignature6( buf ) == 64 ) {
+        return readFirmware6( buf );
       }
     } catch ( IOException e ) {
     } finally {
@@ -125,6 +113,63 @@ class FirmwareUtils
       } catch ( IOException e ) { }
     }
     return 0;
+  }
+
+  //                                   2.1   2.2   2.3   2.4  2.4c   2.5  2.5c  2.51
+  // signatures differ in bytes 7- 6  f834  f83a  f990  fa0a  fe94  fb7e  fc10  fb94
+  //                             -12  08d5  08d5  08d5  08f5  08f5  08d5  08d5  08d5
+  //                           17-16  0c40  0c40  0c50  0c30  0c38  0c40  0c48  0c40
+  private static int readFirmware2( byte[] buf )
+  {
+    if ( buf[7] == (byte)0xf8 ) {       // 2.1 2.2
+      if ( buf[6] == (byte)0x34 ) {     // 2.1
+        return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xf5 )? 2100 : -2100;
+      } else if ( buf[6] == (byte)0x3a ) { // 2.2
+        return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xf5 )? 2200 : -2200;
+      }
+      return -200;
+    } else if ( buf[7] == (byte)0xf9 ) { // 2.3
+      if ( buf[6] == (byte)0x90 ) {
+        return (buf[16] == (byte)0x50 && buf[12] == (byte)0xf5 )? 2300 : -2300;
+      }
+      return -230;
+    } else if ( buf[7] == (byte)0xfa ) { // 2.4
+      if ( buf[6] == (byte)0x0a ) {
+        return ( buf[16] == (byte)0x30 && buf[12] == (byte)0xf5 )? 2400 : -2400;
+      }
+      return -240;
+    } else if ( buf[7] == (byte)0xfb ) { // 2.5  2.51
+      if ( buf[6] == (byte)0x7e ) {      // 2.5
+        return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xd5 )? 2500 : -2500;
+      } else if ( buf[6] == (byte)0x94 ) { // 2.51
+        return ( buf[16] == (byte)0x40 && buf[12] == (byte)0xd5 )? 2501 : -2501;
+      }
+      return -250;
+    } else if ( buf[7] == (byte)0xfc ) { // 2.5c
+      if ( buf[6] == (byte)0x10 ) {
+        return ( buf[16] == (byte)0x48 && buf[12] == (byte)0xd5 )? 2512 : -2512;
+      }
+      return -256;
+    } else if ( buf[7] == (byte)0xfe ) { // 2.4c
+      if ( buf[6] == (byte)0x94 ) {
+        return ( buf[16] == (byte)0x38 && buf[12] == (byte)0xf5 )? 2412 : -2412;
+      }
+      return -246;
+    }
+    return -99; // failed on byte[7]
+  }
+
+  // FIXME it is not clear which bytes can be used to identify a firmware 2.6.X
+  //       neither if this is a suitable block
+  private static int readFirmware6( byte[] buf )
+  {
+    if ( buf[7] == (byte)0x70 ) { // 2.6.3
+      if ( buf[6] == (byte)0x08 ) {
+        return ( buf[16] == (byte)0x03 && buf[12] == (byte)0x08 )? 2630 : -2630;
+      }
+      return -266;
+    }
+    return -99; // failed on byte[7]
   }
 
   static boolean firmwareChecksum( int fw_version, File fp )
@@ -139,6 +184,7 @@ class FirmwareUtils
       case 2412: len = 16504; break;
       case 2501: len = 17540; break;
       case 2512: len = 17792; break;
+      case 2630: len = 25568; break;
     }
     if ( len == 0 ) return false; // bad formware version
     len /= 4; // number of int to read
@@ -166,6 +212,7 @@ class FirmwareUtils
       case 2500: return ( check == 0x463c0306 );
       case 2501: return ( check == 0x20e9a198 ); // 4-calib data double beep
       case 2512: return ( check == 0x1ecb8dc0 ); // continuous
+      case 2630: return ( check == 0x1b1488c5 );
     }
     return false;
   }
@@ -186,9 +233,9 @@ class FirmwareUtils
   //       dis.readFully( buf, 0, 256 );
   //       // Log.v("DistoX", "HARDWARE " + buf[0x40] + " " + buf[0x41] + " " + buf[0x42] + " " + buf[0x43] );
   //       ret = (int)(buf[0x40]) + ((int)(buf[0x41])<<8); // major * 10 + minor
-  //       // FIXME  ((int)(buf[0x42]))<<16 + ((int)(buf[0x43]))<<24; 
+  //       // FIXME  ((int)(buf[0x42]))<<16 + ((int)(buf[0x43]))<<24;
   //     }
-  //   } catch ( IOException e ) { 
+  //   } catch ( IOException e ) {
   //     // TODO
   //   }
   //   return ret;
