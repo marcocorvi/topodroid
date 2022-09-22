ccabb6dbd23bf5cefd88a59909a17cef7ed65759
marcocorvi <marco.corvi@gmail.com>
2022-09-22 11:35:03 +0200

    GM data search on error

---
+++ src/com/topodroid/calib/GMSearchDialog.java
@@ -0,0 +1,118 @@
+/* @file GMSearchDialog.java
+ *
+ * @author marco corvi
+ * @date jul 2017
+ *
+ * @brief TopoDroid search dialog for station-search or leg-flag search
+ *
+ * displays the stack of saved stations and allows to push 
+ * a station on it or pop one from it
+ *
+ * --------------------------------------------------------
+ *  Copyright This software is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ * --------------------------------------------------------
+ */
+package com.topodroid.calib;
+
+// import com.topodroid.utils.TDLog;
+import com.topodroid.utils.TDString;
+import com.topodroid.ui.MyKeyboard;
+import com.topodroid.ui.MyDialog;
+import com.topodroid.prefs.TDSetting;
+import com.topodroid.TDX.TDConst;
+import com.topodroid.TDX.GMActivity;
+import com.topodroid.TDX.R;
+
+import android.os.Bundle;
+// import android.app.Dialog;
+
+import android.content.Context;
+
+import android.view.View;
+// import android.view.View.OnClickListener;
+// import android.view.ViewGroup.LayoutParams;
+import android.inputmethodservice.KeyboardView;
+
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
+
+public class GMSearchDialog extends MyDialog
+                           implements View.OnClickListener
+{
+  private final GMActivity mParent;
+  private EditText mError;
+
+  private Button mBtnSearch;
+  private Button mBtnClear;  // clear search
+
+  private MyKeyboard mKeyboard = null;
+
+  public GMSearchDialog( Context context, GMActivity parent )
+  {
+    super( context, null, R.string.GMSearchDialog ); // null app
+    mParent  = parent;
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) 
+  {
+    super.onCreate(savedInstanceState);
+
+    initLayout( R.layout.gm_search_dialog, R.string.title_gm_search );
+
+    mError = (EditText) findViewById( R.id.error );
+    mError.setText( "1.0" ); // degrees
+
+    mBtnSearch  = (Button) findViewById(R.id.btn_search );
+    mBtnSearch.setOnClickListener( this );
+    mBtnClear  = (Button) findViewById(R.id.btn_clear );
+    mBtnClear.setOnClickListener( this );
+
+    ( (Button) findViewById(R.id.btn_cancel) ).setOnClickListener( this ); // CANCEL
+
+    mKeyboard = new MyKeyboard( mContext, (KeyboardView)findViewById( R.id.keyboardview ), 
+                                R.xml.my_keyboard_base_sign, R.xml.my_keyboard_qwerty );
+    if ( TDSetting.mKeyboard ) {
+      int flag = MyKeyboard.FLAG_POINT_LCASE_2ND;
+      if ( TDSetting.mStationNames == 1 ) flag = MyKeyboard.FLAG_POINT;
+      MyKeyboard.registerEditText( mKeyboard, mError, flag);
+    } else {
+      mKeyboard.hide();
+      if ( TDSetting.mStationNames == 1 ) {
+        mError.setInputType( TDConst.NUMBER_DECIMAL );
+      }
+    }
+  }
+
+  @Override
+  public void onClick(View v) 
+  {
+    MyKeyboard.close( mKeyboard );
+
+    // TDLog.Log(  TDLog.LOG_INPUT, "Search Dialog onClick() " );
+    Button b = (Button) v;
+    if ( b == mBtnSearch ) { // SEARCH 
+      float error = 1.0f; // degrees
+      try { 
+        error = Float.parseFloat( mError.getText().toString().trim() );
+      } catch ( NumberFormatException e ) { }
+      mParent.searchData( error );
+    } else if ( b == mBtnClear ) {
+      mParent.clearSearchResult();
+    // } else if ( b == mBtnCancel ) {
+    //   /* nothing : dismiss */
+    }
+    dismiss();
+  }
+
+  @Override
+  public void onBackPressed()
+  {
+    if ( MyKeyboard.close( mKeyboard ) ) return;
+    dismiss();
+  }
+
+}
---
+++ src/com/topodroid/utils/StringPair.java
@@ -0,0 +1,59 @@
+/* @file StringPair.java
+ *
+ * @author marco corvi
+ * @date nov 2011
+ *
+ * @brief TopoDroid (unordered) pair of strings
+ * --------------------------------------------------------
+ *  Copyright This software is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ * --------------------------------------------------------
+ */
+package com.topodroid.utils;
+
+public class StringPair
+{
+  String first;
+  String second;
+
+  /** cstr
+   * @param f  fisrt string
+   * @param s  second string
+   */
+  public StringPair( String f, String s ) 
+  {
+    if ( f.compareTo(s) < 0 ) {
+      first  = f;
+      second = s;
+    } else {
+      first  = s;
+      second = f;
+    }
+  }
+
+  /** @return true if the string pair is equal to the given string pair
+   * @param sp string pair
+   */
+  public boolean equals( StringPair sp ) { return first.equals( sp.first ) && second.equals( sp.second ); }
+
+  /** @return true if the string pair is equal to the given strings
+   * @param f  fisrt string
+   * @param s  second string
+   */
+  public boolean equals( String f, String s ) 
+  {
+    if ( f.compareTo(s) < 0 ) {
+      return first.equals( f ) && second.equals( s );
+    } else {
+      return first.equals( s ) && second.equals( f );
+    }
+  }
+
+  /** @return string presentation of the string pair
+   */
+  @Override
+  public String toString() 
+  {
+    return "<" + first + " " + second + ">";
+  }
+}
