248e9569c8f0debe329dc83a49f941131e529233
marcocorvi <marco.corvi@gmail.com>
2026-08-22 11:21:26 +0200

    build files, and resources

--- utils/ai_switch.sh
+++ utils/ai_switch.sh
@@ -11,28 +11,60 @@
 # build.xml is a sym-link to the appropriate file. 
 #
 cd /home/programs/android-sdk/samples/android-8/topodroid
+rm libd8 \
+   build.xml \
+   src/com/topodroid/help/AIhelper.java \
+   src/com/topodroid/help/AIdialog.java \
+   src/com/topodroid/help/HelpAIdialog.java \
+   src/com/topodroid/help/HelpDialog.java \
+   src/com/topodroid/help/UserManualActivity.java \
+   src/com/topodroid/prefs/GeminiDialog.java \
+   src/com/topodroid/prefs/PrefAIdialog.java \
+   src/com/topodroid/prefs/TDPrefActivity.java \
+   res/layout/user_manual_activity.xml \
+   res/layout/help_dialog.xml
+rm -f assets/ai/dict.txt \
+      assets/ai/names.txt \
+      assets/ai/setting.txt 
+rm -f res/drawable/iz_ai.png \
+      res/drawable/iz_ai_no.png
+
 if [ "$1" = "yes" ]; then
   echo "turning AI support ON"
-  rm libd8
   ln -s libd8ai libd8
-  rm build.xml
   ln -s build-ai.xml build.xml
-  cd src/com/topodroid/help
-  rm AIhelper.java
-  ln -s ../../../../AIhelp/ai/AIhelper.java .
-  touch AIhelper.java
-  cd -
+  cp ./AIhelp/ai/AIhelper.java src/com/topodroid/help/
+  cp ./AIhelp/ai/AIdialog.java src/com/topodroid/help/
+  cp ./AIhelp/ai/HelpAIdialog.java src/com/topodroid/help/
+  cp ./AIhelp/ai/HelpDialog.java src/com/topodroid/help/
+  cp ./AIhelp/ai/UserManualActivity.java src/com/topodroid/help/
+  cp ./AIhelp/ai/GeminiDialog.java src/com/topodroid/prefs/
+  cp ./AIhelp/ai/PrefAIdialog.java src/com/topodroid/prefs/
+  cp ./AIhelp/ai/TDPrefActivity.java src/com/topodroid/prefs/
+  cp ./AIhelp/ai/user_manual_activity.xml res/layout/
+  cp ./AIhelp/ai/help_dialog.xml res/layout/
+  cp ./AIhelp/ai/dict.txt assets/ai/
+  cp ./AIhelp/ai/names.txt assets/ai/
+  cp ./AIhelp/ai/settings.txt assets/ai/
+  cp ./AIhelp/ai/iz_ai.png res/drawable/
+  cp ./AIhelp/ai/iz_ai_no.png res/drawable/
+  make clean
 elif [ "$1" = "no" ]; then
   echo "turning AI support OFF"
-  rm libd8
   ln -s libd8noai libd8
-  rm build.xml
   ln -s build-noai.xml build.xml
-  cd src/com/topodroid/help
-  rm AIhelper.java
-  ln -s ../../../../AIhelp/no/AIhelper.java .
-  touch AIhelper.java
-  cd -
+  cp ./AIhelp/no/AIhelper.java src/com/topodroid/help/
+  cp ./AIhelp/no/AIdialog.java src/com/topodroid/help/
+  cp ./AIhelp/no/HelpAIdialog.java src/com/topodroid/help/
+  cp ./AIhelp/no/HelpDialog.java src/com/topodroid/help/
+  cp ./AIhelp/no/UserManualActivity.java src/com/topodroid/help/
+  cp ./AIhelp/no/GeminiDialog.java src/com/topodroid/prefs/
+  cp ./AIhelp/no/PrefAIdialog.java src/com/topodroid/prefs/
+  cp ./AIhelp/no/TDPrefActivity.java src/com/topodroid/prefs/
+  cp ./AIhelp/no/user_manual_activity.xml res/layout/
+  cp ./AIhelp/no/help_dialog.xml res/layout/
+  make clean
 else
   echo "Usage $0 [yes|no]"
 fi
+# echo "Remember to do a \"make clean\""
