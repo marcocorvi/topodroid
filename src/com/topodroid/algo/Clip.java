4d22e0fc671fd27cc55c91e40c733f728360aeed
marcocorvi <marco.corvi@gmail.com>
2026-09-02 21:07:20 +0200

    folder algo

---
+++ src/com/topodroid/algo/Clip.java
@@ -0,0 +1,35 @@
+/** @file Scrap.java
+ *
+ * @author marco corvi
+ * @date oct 2019
+ *
+ * @brief TopoDroid clipping algo
+ * --------------------------------------------------------
+ *  Copyright This software is distributed under GPL-3.0 or later
+ *  See the file COPYING.
+ * --------------------------------------------------------
+ */
+package com.topodroid.algo;
+
+// import com.topodroid.util.TDLog;
+import com.topodroid.TDX.ICanvasCommand;
+import com.topodroid.TDX.DrawingLinePath;
+import com.topodroid.TDX.LinePoint;
+
+import java.util.List;
+
+public class Clip
+{
+
+  /** clip a line to the outlines
+   * @param stack     list of scrap lines
+   * @param line      line to clip
+   * @param clip_mode clipping mode (1: "last" end, 2: both ends)
+   */
+  public static void toOutline( List< ICanvasCommand > stack, DrawingLinePath line, int clip_mode )
+  {
+ 
+  }
+
+
+}
